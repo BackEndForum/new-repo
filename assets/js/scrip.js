97fe9125b54c5ae7a9128b7b897c91678944afe7
YourName <youremail@example.com>
2026-01-24 15:36:42 +0400

    edit again on file scrip.js

--- assets/js/scrip.js
+++ assets/js/scrip.js
@@ -1,6 +1,5 @@
 let art = [10, 20, 30, 40];
-for (let i = 0; i < art.length; i++) {
-  let z = 3;
-  z += 2;
-  x[i] = ++z / 4;
-}
+art.forEach((element) => {
+  let one = 1;
+});
+console.log(art[2]);
